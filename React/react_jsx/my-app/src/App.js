ec537a43af0f7935742ac36e8893e5169a056ff6
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2022-07-07 00:55:39 -0500

    first react with jsx

---
+++ React/react_jsx/my-app/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello Dojo! </h1>
+      <h3>Things I need to do</h3>
+      <ul>
+        <li>Learn more React</li>
+        <li>Create more projects</li>
+        <li>Update my Linkedin</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
