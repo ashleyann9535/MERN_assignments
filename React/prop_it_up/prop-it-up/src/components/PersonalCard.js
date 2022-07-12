9e204a85877d0c6bcaade7c6396e9ce1f13b0805
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2022-07-11 22:29:07 -0500

    using state

--- React/prop_it_up/prop-it-up/src/components/PersonalCard.js
+++ React/prop_it_up/prop-it-up/src/components/PersonalCard.js
@@ -1,14 +1,22 @@
 import '../App.css';
+import React, { useState } from 'react';
 
 const PersonalCard = (props) => {
 
     const {name, age, hair} = props;
 
+    const [firstAge, setAge] = useState(age); 
+
+    const handleClick = () =>{
+        setAge(firstAge + 1);
+    }
+
     return (
         <div className = "personalCard">
             <h1>{name}</h1>
-            <p className = "left-align">Age: {age}</p>
+            <p className = "left-align">Age: {firstAge}</p>
             <p className = "left-align">Hair Color: {hair}</p>
+            <button onClick= {handleClick} >Count Age</button>
         </div>
     )
 }
