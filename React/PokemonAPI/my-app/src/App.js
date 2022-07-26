b9d3d0593115cbb817e980d3491c3bde446301c1
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2022-07-26 13:52:45 -0500

    pokemon API with axios

--- React/PokemonAPI/my-app/src/App.js
+++ React/PokemonAPI/my-app/src/App.js
@@ -1,21 +1,27 @@
-import {useState, useEffect} from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
   const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
-      .then(response => response.json())
-      .then(response => setCharacters(response.results))
-  }, [])
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+      .then((response) => {
+        setCharacters(response.data.results);
+      });
+  }, []);
+
+  console.log(characters);
 
   return (
     <div className="App">
       <h1>Pokemon API</h1>
-      {characters.length > 0 && characters.map((character) => {
-        return (<p> {character.name} </p>)
-      })}
+      {characters.length > 0 &&
+        characters.map((character) => {
+          return <p> {character.name} </p>;
+        })}
     </div>
   );
 }
